ba77a793458a54a6fd70179c9596dec74fcf5d99
igavelyuk <gavelyukworking@gmail.com>
2019-10-26 21:20:25 +0300

    added tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,32 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Menu from '@/components/Menu.vue'
+import Main from '@/components/Main.vue'
+import Footer from '@/components/Footer.vue'
 
-describe('HelloWorld.vue', () => {
+describe('Menu.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(Menu, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
+
+describe('Main.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(Main, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
+
+describe('Footer.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(Footer, {
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
