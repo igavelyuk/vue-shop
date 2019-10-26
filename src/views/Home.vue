8c123ca1bf2bef16ddc5bfaf3fa8dcfe4ecb00bd
igavelyuk <gavelyukworking@gmail.com>
2019-10-26 15:01:18 +0300

    small changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <Menu msg="This is a menu component"/>
     <Main msg="This is a main component"/>
     <Footer msg="This is a footer component"/>
