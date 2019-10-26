79feb6525b890935707be870328e59505c2ee449
igavelyuk <gavelyukworking@gmail.com>
2019-10-26 08:12:42 +0300

    basic files structure update 2

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="footer"
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.footer {
+background:#fac0c0;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="menu"
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.menu {
+background:#c0fac0;
+}
+</style>
