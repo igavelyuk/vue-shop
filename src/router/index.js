633d173de62953ba07001a19fe59a73ea34b8a42
Dave <salazardave93@gmail.com>
2019-10-26 15:00:04 -0500

    refactor layout, lazy load Home component

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
   },
   {
     path: '/about',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,16 @@
 <template>
   <div class="home">
-    <Menu msg="This is a menu component"/>
-    <Main msg="This is a main component"/>
     <Table></Table>
-    <Footer msg="This is a footer component"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Menu from '@/components/Menu.vue'
-import Main from '@/components/Main.vue'
-import Footer from '@/components/Footer.vue'
 import Table from '@/components/Table.vue'
 
 export default {
   name: 'home',
   components: {
-    Menu,
-    Main,
-    Footer,
     Table
   }
 }
