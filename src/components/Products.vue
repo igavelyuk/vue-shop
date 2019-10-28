37d07e67f43531605f29e4aab6f0745647f63c3c
igavelyuk <gavelyukworking@gmail.com>
2019-10-28 17:04:13 +0200

    Products.vue added

---
+++ src/components/Products.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="product">
+  <div v-bind:key="el.id" v-for = "el in products">
+    <h3>{{el.title}}</h3>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props: ['products']
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
