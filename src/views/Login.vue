07064f58612d408bbccc9645d71f6585a1cb9752
igavelyuk <gavelyukworking@gmail.com>
2019-11-22 21:10:52 +0200

    added Login vue and Signup vue files

---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="columns is-centered">
+    <div class="column is-three-fifths">
+      <section>
+        <b-checkbox v-model="hasError">Show errors remain for easy to remmember how it works</b-checkbox>
+        <b-field label="Username"
+        :type="{ 'is-danger': hasError }"
+        :message="{ 'Username is not available': hasError }">
+        <b-input value="johnsilver" maxlength="30"></b-input>
+        </b-field>
+        <b-field label="Password"
+        :type="{ 'is-danger': hasError }"
+        :message="[
+        { 'Password is too short': hasError },
+        { 'Password must have at least 8 characters': hasError }
+        ]">
+        <b-input value="123" type="password" maxlength="30"></b-input>
+      </b-field>
+      <b-button type="is-primary">Log In</b-button>
+    </section>
+  </div>
+</div>
+</template>
+<script>
+export default {
+  name: 'login',
+  props: {
+    msg: {
+      type: String,
+      required: false
+    }
+  },
+  data () {
+    return {
+      hasError: true
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/views/Signup.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="columns is-centered">
+    <div class="column is-three-fifths">
+      <section>
+        <b-field label="Name">
+          <b-input value="Kevin Garvey"></b-input>
+        </b-field>
+
+        <b-field label="Email"
+          type="is-danger"
+          message="This email is invalid">
+        <b-input type="email"
+          value="john@"
+          maxlength="30">
+        </b-input>
+        </b-field>
+
+        <b-field label="Username"
+          type="is-success"
+          message="This username is available">
+        <b-input value="johnsilver" maxlength="30"></b-input>
+        </b-field>
+        <b-field label="Password"
+          type="is-warning"
+          :message="['Password is too short', 'Password must have at least 8 characters']">
+        <b-input value="123" type="password" maxlength="30"></b-input>
+        </b-field>
+        <b-field label="Subject">
+          <b-select placeholder="Select a subject">
+            <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+              </b-select>
+          </b-field>
+        </section>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'register',
+  props: {
+    msg: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
